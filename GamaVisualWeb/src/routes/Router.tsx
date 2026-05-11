28ae211d7c975a683803cb6d4afc2b27eb1cdac7
felixgalvin <felixgalvin1709@gmail.com>
2026-05-11 19:34:06 +0700

    Second Commit, set up routes environment

---
+++ GamaVisualWeb/src/routes/Router.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+
+export function Router() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* public Routes */}
+                <Route path="/" element={<HomePage/>} />
+                
+            </Routes>
+        </BrowserRouter>
+    );
+
+}
