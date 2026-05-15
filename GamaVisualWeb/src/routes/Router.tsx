396c67faff28cea598a2377cb526fae52a30f2b6
felixgalvin <felixgalvin1709@gmail.com>
2026-05-15 16:12:08 +0700

    First Progress Singer Profile, undone

--- GamaVisualWeb/src/routes/Router.tsx
+++ GamaVisualWeb/src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import QuoteAlbumPage from "../pages/QuoteAlbumPage";
+import SingerProfile from "../pages/SingerProfile";
 
 export function Router() {
 
@@ -10,9 +11,9 @@ export function Router() {
                 {/* public Routes */}
                 <Route path="/" element={<HomePage />} />
                 <Route path="/quotes" element={<QuoteAlbumPage />} />
-                {/* <Route path="/album" element={< Almbum/>} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/about" element={<About />} />
+                {/* <Route path="/album" element={< Almbum/>} />*/}
+                <Route path="/profile" element={<SingerProfile />} />
+                {/* <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} /> */}
             </Routes>
         </BrowserRouter>
